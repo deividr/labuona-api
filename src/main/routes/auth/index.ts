a6d12401e525d28a308c12248dc8c8b716f79e3c
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2023-10-24 21:36:15 -0300

    chore: update packages and improve config tests

--- src/main/routes/auth/index.ts
+++ src/main/routes/auth/index.ts
@@ -2,7 +2,7 @@ import { FastifyPluginAsync } from "fastify";
 
 const auth: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get("/", async function (request, reply) {
-    return "This is auth routes...";
+    return "this is an auth";
   });
 };
 
