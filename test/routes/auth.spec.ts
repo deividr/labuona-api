befc72f88d90669fc2c0cfe93fc322f92582324f
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2023-11-02 12:42:46 -0300

    feat: ensure return 200 when call /auth/login endpoint with valid credentials

--- test/routes/auth.spec.ts
+++ test/routes/auth.spec.ts
@@ -1,14 +1,41 @@
 import { test } from "tap";
 import { build } from "../helper";
 import { db } from "../../src/infra/db/postgres/database";
+import { faker } from "@faker-js/faker";
+import { User } from "../../src/infra/db/kysely/types/user";
+import { NodeCrypto } from "../../src/infra/crypto/node-crypto-adapter";
+
+const mockUser = async (): Promise<{ user: User; rawPassword: string }> => {
+  const hasher = new NodeCrypto();
+  const rawPassword = faker.internet.password();
+  const password = await hasher.hash(rawPassword);
+
+  const newUser = await db
+    .insertInto("users")
+    .values({
+      username: faker.internet.userName(),
+      password: password,
+      name: faker.person.fullName(),
+      isDeleted: false,
+    })
+    .returningAll()
+    .executeTakeFirst();
+
+  return { user: newUser as User, rawPassword };
+};
 
 test("Auth", async (t) => {
-  t.test("auth is loaded", async (t) => {
+  t.test("should return 200 if credentials are valid", async (t) => {
+    const { user, rawPassword } = await mockUser();
+
     const app = await build(t);
 
-    await app.inject({
-      url: "/auth/login",
+    const response = await app.inject({
+      url: `/auth/login?username=${user.username}&password=${rawPassword}`,
     });
+
+    t.equal(response.statusCode, 200);
+    t.hasProp(JSON.parse(response.body), "token");
   });
 
   t.teardown(async () => {
